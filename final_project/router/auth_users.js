f7da0cbf137ca5f3f8a65992b54d529ebf64f553
Saraicd <saraicdomingues@gmail.com>
2025-04-11 15:07:05 +0100

    Task 8

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -23,6 +23,11 @@ regd_users.post("/login", (req,res) => {
 
   const token = jwt.sign({ username }, "fingerprint_customer", { expiresIn: "1h" });
 
+  req.session.authorization = {
+    accessToken: token,
+    username: username
+  };
+
   return res.status(200).json({
     message: "Login successful",
     token,
@@ -31,8 +36,27 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { isbn } = req.params;
+  const { review } = req.query;
+
+  if (!review) return res.status(400).json({ message: "Review must be provided" });
+console.log(req.session)
+  const username = req.session.authorization.username;
+  if (!username) return res.status(403).json({ message: "You must be logged in to post a review" });
+
+  const book = books[isbn];
+  if (!book) return res.status(404).json({ message: "Book not found" });
+
+  if (book.reviews && book.reviews[username]) {
+    book.reviews[username] = review; // Modify the existing review
+    return res.status(200).json({ message: `Review for book ${book.title} updated successfully` });
+  }
+
+  if (!book.reviews) book.reviews = {};
+
+  book.reviews[username] = review;
+
+  return res.status(200).json({ message: `Review for book ${book.title} added successfully` });
 });
 
 module.exports.authenticated = regd_users;
