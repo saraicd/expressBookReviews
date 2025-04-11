274f0a005c8ff7a11141cdd9f6d60a78142ef27d
Saraicd <saraicdomingues@gmail.com>
2025-04-11 13:02:38 +0100

    Task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -53,8 +53,13 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { isbn } = req.params;
+  const bookByIsbn = books[isbn];
+
+  if(!bookByIsbn) return res.status(404).json({message: "Book not found"});
+  if(!bookByIsbn.reviews || Object.keys(bookByIsbn.reviews).length === 0) return res.status(404).json({message: "There are no reviews for this book"});
+  const bookReviews = Object.values(bookByIsbn.reviews);
+  return res.status(200).json(bookReviews);
 });
 
 module.exports.general = public_users;
