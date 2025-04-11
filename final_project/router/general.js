759281215461412dd6220edb837fd8938ffb65aa
Saraicd <saraicdomingues@gmail.com>
2025-04-11 12:29:48 +0100

    Task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,10 +24,13 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+  const { isbn } = req.params ?? "";
+  const bookByIsbn = books[isbn];
+
+  if(bookByIsbn) return res.status(200).json(bookByIsbn);
+  return res.status(404).json({message: "Book was not found"});
+});
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
